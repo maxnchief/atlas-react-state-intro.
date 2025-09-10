4fb5d7deefadc1c708673af7e673dfca13e9a53a
maxnchief <logandogan@gmail.com>
2025-09-10 14:27:56 -0500

    Update Header component to display the number of enrolled classes dynamically

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,10 +1,15 @@
+import { useContext } from "react";
 import logo from "./assets/logo.png";
+import { EnrolledCoursesContext } from "./App";
 
 export default function Header() {
+  const { enrolledCourses } = useContext(EnrolledCoursesContext);
   return (
     <div className="header">
       <img src={logo} alt="logo" className="logo" />
-      <div className="enrollment">Classes Enrolled: 1</div>
+      <div className="enrollment">
+        Classes Enrolled: {enrolledCourses.length}
+      </div>
     </div>
   );
 }
